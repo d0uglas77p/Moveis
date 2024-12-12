e99c3e2dc89cf46d0edafefd5b45a8d74db4f62b
d0uglas77p <douglas_1313@hotmail.com>
2024-12-12 00:12:20 -0300

    updade

--- src/main/java/com/mycoding/bean/Navegacao.java
+++ src/main/java/com/mycoding/bean/Navegacao.java
@@ -1,4 +1,14 @@
 package com.mycoding.bean;
 
+import javax.faces.bean.ManagedBean;
+import javax.faces.view.ViewScoped;
+
+@ManagedBean
+@ViewScoped
 public class Navegacao {
+
+    public String irMoveis() {
+        return "/paginas/moveis.xhtml?faces-redirect=true";
+    }
+
 }
